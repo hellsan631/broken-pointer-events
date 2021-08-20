b2c975db16a6cdc52b71805860e36e400ca9e8f6
hellsan631 <matkle414@gmail.com>
2021-08-19 21:55:02 -0700

    remove reanimated

--- App.js
+++ App.js
@@ -1,35 +1,10 @@
-import Animated, {
-  useSharedValue,
-  useAnimatedGestureHandler,
-  useAnimatedStyle,
-} from "react-native-reanimated";
 import { View, Button } from "react-native";
 import React from "react";
 import { PanGestureHandler } from 'react-native-gesture-handler';
 
 export default function AnimatedStyleUpdateExample(props) {
-  const x = useSharedValue(0);
-
-  const gestureHandler = useAnimatedGestureHandler({
-    onStart: (_, ctx) => {
-      ctx.startX = x.value;
-    },
-    onActive: (event, ctx) => {
-      x.value = ctx.startX + event.translationX;
-    },
-  });
 
   const square = { width: 100, height: 100, backgroundColor: "black" }
-
-  const animatedStyle = useAnimatedStyle(() => {
-    return {
-      transform: [
-        {
-          translateX: x.value,
-        },
-      ],
-    };
-  });
 
   return (
     <View
@@ -40,13 +15,12 @@ export default function AnimatedStyleUpdateExample(props) {
         flexDirection: "column",
       }}
     >
-      <PanGestureHandler onGestureEvent={gestureHandler}>
-        <Animated.View
-          style={animatedStyle}
+      <PanGestureHandler onGestureEvent={console.log} activeOffsetX={[0,0]}>
+        <View
           pointerEvents="box-none"
         >
           <View style={[square]} />
-        </Animated.View>
+        </View>
       </PanGestureHandler>
     </View>
   );
